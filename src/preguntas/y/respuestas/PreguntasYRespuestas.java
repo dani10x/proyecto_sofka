1ce91759fef4a8f6e323319f7bd777085920342e
USER <USER@DESKTOP-FRAM75J>
2022-04-26 14:11:52 -0500

    v0.1

---
+++ src/preguntas/y/respuestas/PreguntasYRespuestas.java
@@ -0,0 +1,17 @@
+package preguntas.y.respuestas;
+
+/**
+ *
+ * @author Daniel Alzate
+ */
+public class PreguntasYRespuestas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola");
+        System.out.println("");
+    }
+    
+}
