00c81913216010c52ce9a6df7aa1040bd929d7b8
Daniel Alzate <104457674+dani10x@users.noreply.github.com>
2022-04-26 17:48:25 -0500

    Revert "Delete Pregunta.java"
    This reverts commit b120d74a22e7250b214839f25386feb348584dd3.

---
+++ src/preguntas/y/respuestas/clases/Pregunta.java
@@ -0,0 +1,19 @@
+package preguntas.y.respuestas.clases;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Daniel Alzate
+ */
+public final class Pregunta {
+    private String pregunta;
+    private ArrayList respuestas;
+
+    public Pregunta(String pregunta, ArrayList respuestas) {
+        this.pregunta = pregunta;
+        this.respuestas = respuestas;
+    }
+    
+    
+}
