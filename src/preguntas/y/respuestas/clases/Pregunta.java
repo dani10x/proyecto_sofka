e05fad1b158b08af2264244f49723961c9f9dd28
USER <USER@DESKTOP-FRAM75J>
2022-04-26 17:30:39 -0500

    v0.13

--- src/preguntas/y/respuestas/clases/Pregunta.java
+++ src/preguntas/y/respuestas/clases/Pregunta.java
@@ -7,13 +7,37 @@ import java.util.ArrayList;
  * @author Daniel Alzate
  */
 public final class Pregunta {
+    private int categoria;
     private String pregunta;
     private ArrayList respuestas;
 
-    public Pregunta(String pregunta, ArrayList respuestas) {
+    public Pregunta(int categoria, String pregunta, ArrayList respuestas) {
+        this.categoria=categoria;
         this.pregunta = pregunta;
         this.respuestas = respuestas;
     }
-    
-    
+
+    public int getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(int categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getPregunta() {
+        return pregunta;
+    }
+
+    public void setPregunta(String pregunta) {
+        this.pregunta = pregunta;
+    }
+
+    public ArrayList getRespuestas() {
+        return respuestas;
+    }
+
+    public void setRespuestas(ArrayList respuestas) {
+        this.respuestas = respuestas;
+    }
 }
