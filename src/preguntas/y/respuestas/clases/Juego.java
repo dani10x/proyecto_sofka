72d398eb5208d7f9b74b11f3d5b9693ae2e76d94
USER <USER@DESKTOP-FRAM75J>
2022-04-26 17:31:27 -0500

    v0.14

---
+++ src/preguntas/y/respuestas/clases/Juego.java
@@ -0,0 +1,17 @@
+package preguntas.y.respuestas.clases;
+
+/**
+ *
+ * @author Daniel Alzate
+ */
+public final class Juego {
+    private final String id_jugador;
+    private final int dinero;
+
+    public Juego(String id_jugador, int dinero) {
+        this.id_jugador = id_jugador;
+        this.dinero = dinero;
+    }
+    
+    
+}
